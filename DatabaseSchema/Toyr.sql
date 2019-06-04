dcae2778cd5d4e5e2b9f675bd656414735506193
Cernat Iulian <iulian.cernat98@yahoo.com>
2019-06-04 20:19:07 +0300

    Am facut niste modificari la baza de date si am organizat codul pe MVC

--- DatabaseSchema/Toyr.sql
+++ DatabaseSchema/Toyr.sql
@@ -42,9 +42,9 @@ CREATE TABLE `product_images`
 	`id` int PRIMARY KEY AUTO_INCREMENT,
     `product_id` int NOT NULL,
     `name` varchar(255),
-    `path` varchar(255),
-    `date_created` timestamp,
-    `date updated` timestamp
+    `front_img_flag` boolean,
+    `created_at` timestamp,
+    `updated_at` timestamp
 );
 CREATE TABLE `product_price`
 (
