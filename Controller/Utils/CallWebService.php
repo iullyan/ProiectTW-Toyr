713d5e1b96d887d28b7fbeb84613472feb7c4e4e
Cernat Iulian <iulian.cernat98@yahoo.com>
2019-06-15 23:01:40 +0300

    Am adaugat suport pentru apelarea Serviciului Web

---
+++ Controller/Utils/CallWebService.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class CallWebService
+{
+
+    //returns JSON
+    public function doGet($url)
+    {
+        $request = curl_init();
+
+        curl_setopt($request, CURLOPT_URL, $url);
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($request, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($request);
+        curl_close($request);
+        $data = json_decode($response);
+
+        return $data;
+    }
+
+    public function doPost($url, $JSONdata)
+    {
+        $ch = curl_init($url);
+
+        $payload = $JSONdata;
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+
+        return $result;
+    }
+}
