4ebf5818a073e29b17c17ec11e93575faf9b3af4
Cernat Iulian <iulian.cernat98@yahoo.com>
2019-06-17 12:22:42 +0300

    Terminat fluxul RSS.

--- Controller/RSSFeeds/NewestProducts.php
+++ Controller/RSSFeeds/NewestProducts.php
@@ -16,7 +16,7 @@ $orderBy = "orderBy=new&offset=0";
 $url = WEB_CONST_URL_PART . "Product/getProducts.php?" . $orderBy;
 $newestProductsList = $webService->doGet($url);
 
-$xml = new DomDocument("1.0","UTF-8");
+$xml = new DomDocument("1.0", "UTF-8");
 $xml->formatOutput = true;
 $rss = $xml->createElement("rss");
 $xml->appendChild($rss);
@@ -35,10 +35,13 @@ $rss->appendChild($language);
 $lastBuildDate = $xml->createElement("lastBuildDate", $lastBuildDate);
 $rss->appendChild($lastBuildDate);
 
-if ( ! array_key_exists('Message', $newestProductsList)) {
+if (!array_key_exists('Message', $newestProductsList)) {
 
     $records = $newestProductsList->records;
     foreach ($records as $product) {
+        $discountData = $product->discount;
+        $promotionsList = $product->promotions;
+
         $item = $xml->createElement("item");
         $id = $xml->createElement("id", $product->product->id);
         $item->appendChild($id);
@@ -73,12 +76,58 @@ if ( ! array_key_exists('Message', $newestProductsList)) {
         $createdAt = $xml->createCDATASection($product->product->created_at);
         $item->appendChild($createdAt);
 
+
+        if ($discountData) {
+
+            $discount = $xml->createElement("discount");
+            $discountPercentage = $xml->createElement("discountPercentage");
+            $discountPercentage = $xml->createCDATASection($discountData->discount_percentage);
+            $discount->appendChild($discountPercentage);
+
+            $priceWithDiscount = $xml->createElement("priceWithDiscount");
+            $priceWithDiscount = $xml->createCDATASection($discountData->price_with_discount);
+            $discount->appendChild($priceWithDiscount);
+            $item->appendChild($discount);
+        } else {
+            $discount = $xml->createElement("discount", "false");
+            $item->appendChild($discount);
+
+        }
+
+        if ($promotionsList) {
+                $promotions = $xml->createElement("promotions");
+                foreach ($promotionsList as $promotionData) {
+                    $promotion = $xml->createElement("promotion");
+                        $giftedProductId = $xml->createElement("giftedProductid", $promotionData->gifted_product_id);
+                        $promotion->appendChild($giftedProductId);
+
+                        $productUnitsBought = $xml->createElement("productUnitsBought", $promotionData->gifted_product_quantity);
+                        $promotion->appendChild($productUnitsBought);
+
+                        $giftedProductQuantity = $xml->createElement("giftedProductQuantity", $promotionData->gifted_product_quantity);
+                        $promotion->appendChild($giftedProductQuantity);
+
+                        $validFrom = $xml->createElement("validFrom", $promotionData->valid_from);
+                        $promotion->appendChild($validFrom);
+
+                        $validUntil = $xml->createElement("validUntil", $promotionData->valid_until);
+                        $promotion->appendChild($validUntil);
+
+
+                        $promotions->appendChild($promotion);
+                }
+                $item->appendChild($promotions);
+        }
+        else {
+            $promotions = $xml->createElement("promotions", "false");
+            $item->appendChild($promotions);
+        }
+
         $rss->appendChild($item);
+
 
     }
 }
 
-
-$xml->save('E:\test.xml');
 echo $xml->saveXML();
 ?>
