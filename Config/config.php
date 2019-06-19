57cf6ecc6ee7e96a88ad6d6bac1f9ebbacefa280
Madalin Mihai <mihai.madalin04@gmail.com>
2019-06-19 10:20:07 +0300

    Merge branch 'master' of C:\xampp\htdocs\ProiectTW-Toyr with conflicts.

--- Config/config.php
+++ Config/config.php
@@ -1,23 +1,3 @@
 <?php
-//Web service url
-define('PORT', 9999);
-define('WEB_CONST_URL_PART', 'http://localhost:'.PORT.'/ProiectTW-Toyr/WebServices/StoreManagement/Controller/');
-
-//Number of products for RSS feeds
-define('RSS_FEED_NR_OF_PRODUCTS', 100);
-
-//Product page
-define('PRODUCT_PAGE', 'http://localhost:'.PORT.'/ProiectTW-Toyr/View/pages/product.php');
-
-//RSS links
-
-define('NEWEST_PRODUCTS', 'http://localhost:'.PORT.'/ProiectTW-Toyr/Controller/RSSfeeds/NewestProducts.php');
-define('MOST_SOLD_PRODUCTS', 'http://localhost:'.PORT.'/ProiectTW-Toyr/Controller/RSSfeeds/MostSoldProducts.php');
-define('SPECIAL_OFFERS', 'http://localhost:'.PORT.'/ProiectTW-Toyr/Controller/RSSfeeds/SpecialOffers.php');
-
-//Images
-define('FRONT_IMAGES', 'http://localhost:'.PORT.'/ProiectTW-Toyr/Resources/specialProducts/');
-define('IMAGES_LOCATION', 'http://localhost:'.PORT.'/ProiectTW-Toyr/Resources/productImages/');
-
-define ("PRODUCT_ORDERBY", serialize (array ('discount', 'nrSold', 'priceAsc', 'priceDesc', 'new', 'promotion')));
-
+define('WEB_CONST_URL_PART', 'http://localhost:80/ProiectTW-Toyr/WebServices/StoreManagement/Controller/');
+?>
