eb57384e03b860e74d0098dac0dbcd50f8684d43
Madalin Mihai <mihai.madalin04@gmail.com>
2019-06-23 20:13:23 +0300

    Mici modificari la addToCart.php

--- Controller/Dispatcher/addToCart.php
+++ Controller/Dispatcher/addToCart.php
@@ -2,27 +2,34 @@
 
 $productIds = array();
 $productNames = array();
+$productPrices = array();
 
 //get the name and id of the item added to cart
-if (isset($_POST['productId']) && isset($_POST['productName'])) {
+if (isset($_POST['productId']) && isset($_POST['productName']) && isset($_POST['productPrice'])) {
     $productId = $_POST['productId'];
-    $prodctName = $_POST['productName'];
+    $productName = $_POST['productName'];
+    $productPrice = $_POST['productPrice'];
 }
 else
     die("Unspecified parameters");
 
 //if the cart has other items, get the arrays with the items
-if (isset($_COOKIE['cartIds']) && isset($_COOKIE['cartNames'])) {
+if (isset($_COOKIE['cartIds']) && isset($_COOKIE['cartNames']) && isset($_COOKIE['productPrice'])) {
     $productIds = unserialize($_COOKIE['cartIds']);
     $productNames = unserialize($_COOKIE['cartNames']);
+    $productPrices = unserialize($_COOKIE['productPrice']);
 }
 
 //add the item's details to the arrays
-array_push($productNames, $prodctName);
 array_push($productIds,$productId);
+array_push($productNames, $productName);
+array_push($productPrices, $productPrice);
 
-//add arrays back to cookies
+//add arrays back to cookies - WIP
 
+//small test for data
 print_r($productIds);
 echo ' ';
 print_r($productNames);
+echo ' ';
+print_r($productPrices);
